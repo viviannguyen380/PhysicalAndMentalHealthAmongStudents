5e8aa309bfa42fb7f640d5d412919bb60b3c5103
Vivian Nguyen <viviannguyen380@yahoo.com>
2024-05-28 22:03:19 -0700

    new files

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "#8D37B8";
+  }
+
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = '#cc99ff';
+  }
+function myFunction(){
+  document.getElementById("final").innerHTML = "The correct answer is: 3-4 days per week.";
+}
+function myFunction1(){
+  document.getElementById("final1").innerHTML = "The correct answer is: At least 30 minutes per day.";
+}
+
+
+
